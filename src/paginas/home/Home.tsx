4dc17c6b0d466e3699df37bd4c566ef51354e14a
guilhermerubrae <guilhermerubrae@gmail.com>
2024-02-14 13:06:05 -0300

    css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://www.one-stack.jp/wp-content/uploads/2022/05/Discussion-rafiki.png" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
