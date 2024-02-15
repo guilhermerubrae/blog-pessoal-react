75fe557fc8f039e2d01053feac4fdec8c672f603
guilhermerubrae <guilhermerubrae@gmail.com>
2024-02-15 17:44:45 -0300

    home - tarefa - contador

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import Home from './paginas/home/Home';
 import './App.css';
-
+import Tarefa from './paginas/tarefa/Tarefa';
+import Contador from './paginas/contador/Contador';
+import Home from './paginas/home/Home';
 
 function App() {
-  return (
-   <Home />
+   return (
+    <>
+      <Home titulo='Componente '
+      texto='Este é um componente Home que recebe props.' />
+
+      <Contador />
+    
+      <Tarefa />
+    </>
+
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Home.css';
-
-function Home(){
-    return (
-        <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://www.one-stack.jp/wp-content/uploads/2022/05/Discussion-rafiki.png" alt="Imagem Tela Inicial" className="img"/>
-        </>
-    );
-}
-
-export default Home;
+interface Props{
+    titulo: string;
+    texto: string;
+  }
+  
+  function Home(props : Props) {
+    return(
+      <>
+      <h1 className="titulo">{props.titulo}Home</h1>
+      <p>{props.texto}</p>  
+      <img src="https://www.one-stack.jp/wp-content/uploads/2022/05/Discussion-rafiki.png" alt="Imagem Tela Inicial" className="img"/>
+  
+      </>
+    )
+  }
+  
+  export default Home;
